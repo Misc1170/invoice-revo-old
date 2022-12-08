42a50823331aa22942835c9d5286b44402b18f2a
vansilich <kulpovvvan@gmail.com>
2022-12-08 15:33:50 +0300

    Do not upload file if error

--- upload.php
+++ upload.php
@@ -25,14 +25,14 @@ if(!isset($data['filename']) || $data['filename'] == ''){
 }
 
 $pay_link = "";
-//Формируем ссылку на оплату
+//Формируем ссылку на оплату, если необходимо
 if (isset($data['link']) && $data['link'] != '') {
     $pay_link = generatePaylink($data['link']);
 }
 
-//Проверка на существование заказа.
+// Проверка на существование заказа.
 // Раньше при каждой отправке заказа создавалась отдельная запись в базе и файлы. Поэтому для экономии места
-// и правильной работы, мы теперь обновляем послеждний добавленные заказ с таким же order_id
+// и правильной работы, мы теперь обновляем последний добавленный заказ с таким же order_id и потом отдаем ссылку на него же
 $is_order_exists_query = $mysqli->prepare("SELECT * FROM `pdf_uploads` WHERE `order_id` = ? ORDER BY id DESC LIMIT 1");
 $is_order_exists_query->bind_param("s", $data['order_id']);
 $is_order_exists_query->execute();
@@ -49,76 +49,104 @@ if($existing_order !== null){
 $path = $hash . '/' . $data['filename'];
 $link = 'https://fluid-line.ru/invoice924' . $hash;
 
-// Удяляем прошлый архив к заказу, если у них разное имя (так как новый файл не перезапишет старый)
-if($existing_order !== null && $path !== $existing_order['path']){
-    $FileService->delete($existing_order['path']);
-}
+//Проверка на то, что новый заказ уже был оплачен до этого и выставлен ошибочно
+$query = $mysqli->prepare('SELECT `is_paid` FROM `pdf_uploads` WHERE `order_id` = ? ORDER BY id DESC LIMIT 1');
+$query->bind_param('s', $data['order_id']);
+$query->execute();
+$is_paid_result = $query->get_result()->fetch_assoc();
 
-$FileService->upload($path, base64_decode($data['file']));
-if(!$FileService->doesObjectExists($path)){
-    throw new Exception("Не удалось создать файл '$path'");
-}
-
-unset($data['file']);
-
-$filesize = $FileService->headObject($path)['ContentLength'];
-
-$is_paid = 0;
+$is_paid = isset($is_paid_result['is_paid']) ? $is_paid_result['is_paid'] : 0;
 $entity = isset($data['entity']) && $data['entity'] == true ? 1 : 0;
 $pay_block = isset($data['pay_block']) && $data['pay_block'] == true ? 1 : 0;
 $request_link = htmlspecialchars($data['link']);
 
-// Если уже есть заказ, то обновляем его, либо инсертим
-if($existing_order !== null){
-    $result_query = $mysqli->prepare('
-        UPDATE `pdf_uploads` 
-        SET path = ?, `size` = ?, `entity` = ?, `pay_block` = ?, `pay_link` = ?, `email_hash` = ?, `link` = ?
-        WHERE `hash` = ?
-    ');
+$mysqli->begin_transaction();
+try{
 
-    $result_query->bind_param("siiissss",
-        $path, $filesize, $entity, $pay_block, $pay_link, $data['email_hash'], $request_link, $hash
-    );
+    // Если уже есть заказ, то обновляем его, либо инсертим
+    if($existing_order !== null){
+        $result_query = $mysqli->prepare('
+            UPDATE `pdf_uploads` 
+            SET path = ?, `entity` = ?, `pay_block` = ?, `pay_link` = ?, `email_hash` = ?, `link` = ?
+            WHERE `hash` = ?
+        ');
+
+        $result_query->bind_param("siissss",
+            $path, $entity, $pay_block, $pay_link, $data['email_hash'], $request_link, $hash
+        );
+
+        file_put_contents($output_log_dir."/".$log_file, "
+            UPDATE `pdf_uploads` 
+            SET path = '$path', `entity` = $entity, `pay_block` = $pay_block, `pay_link` = '$pay_link', `email_hash` = '$data[email_hash]', `link` = '$request_link'
+            WHERE `hash` = '$hash'
+        ");
+
+        // Удяляем прошлый архив к заказу, если у них разное имя (так как новый файл не перезапишет старый)
+        if($path !== $existing_order['path']){
+            $FileService->delete($existing_order['path']);
+        }
+
+    } else {
+        $result_query = $mysqli->prepare('
+            INSERT INTO `pdf_uploads` 
+            (`is_paid`, `path`, `hash`, `entity`, `order_id`, `pay_block`, `pay_link`, `InvoiceId`, `email_hash`, `link`)
+                VALUES 
+            (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        ');
+        $db_save_link = htmlspecialchars($data['link']);
+        $result_query->bind_param("issisisiss", 
+            $is_paid, 
+            $path, 
+            $hash,
+            $entity,
+            $data['order_id'],
+            $pay_block,
+            $pay_link, 
+            $data['InvoiceId'],
+            $data['email_hash'],
+            $request_link
+        );
+
+        file_put_contents($output_log_dir."/".$log_file, "
+            INSERT INTO `pdf_uploads` 
+            (`is_paid`, `path`, `hash`, `entity`, `order_id`, `pay_block`, `pay_link`, `InvoiceId`, `email_hash`, `link`)
+                VALUES 
+            ($is_paid, '$path', '$hash', $entity, $data[order_id], $pay_block, '$pay_link', $data[InvoiceId], '$data[email_hash]', '$db_save_link')
+        ");
+    }
+
+    $result = $result_query->execute();
+
+    // сохраняем архив заказа
+    $FileService->upload($path, base64_decode($data['file']));
+    if(!$FileService->doesObjectExists($path)){
+        throw new Exception("Не удалось создать файл '$path'");
+    }
+
+    $filesize = $FileService->headObject($path)['ContentLength'];
+    
+    $set_size_query = $mysqli->prepare('UPDATE `pdf_uploads` SET `size` = ? WHERE `hash` = ?');
+    $set_size_query->bind_param("is", $filesize, $hash);
+    $set_size_query->execute();
 
     file_put_contents($output_log_dir."/".$log_file, "
-        UPDATE `pdf_uploads` 
-        SET path = '$path', `size` = $filesize, `entity` = $entity, `pay_block` = $pay_block, `pay_link` = '$pay_link', `email_hash` = '$data[email_hash]', `link` = '$request_link'
-        WHERE `hash` = '$hash'
+        \n
+        UPDATE `pdf_uploads` SET `size` = $filesize WHERE `hash` = '$hash'
     ");
 
+    $mysqli->commit();
 
-} else {
-    $result_query = $mysqli->prepare('
-        INSERT INTO `pdf_uploads` 
-        (`is_paid`, `path`, `hash`, `size`, `entity`, `order_id`, `pay_block`, `pay_link`, `InvoiceId`, `email_hash`, `link`)
-            VALUES 
-        (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-    ');
-    $db_save_link = htmlspecialchars($data['link']);
-    $result_query->bind_param("issiisisiss", 
-        $is_paid, 
-        $path, 
-        $hash,
-        $filesize,
-        $entity,
-        $data['order_id'],
-        $pay_block,
-        $pay_link, 
-        $data['InvoiceId'],
-        $data['email_hash'],
-        $request_link
-    );
+} catch(\Exception $e){
+    $mysqli->rollback();
 
-    file_put_contents($output_log_dir."/".$log_file, "
-        INSERT INTO `pdf_uploads` 
-        (`is_paid`, `path`, `hash`, `size`, `entity`, `order_id`, `pay_block`, `pay_link`, `InvoiceId`, `email_hash`, `link`)
-            VALUES 
-        ($is_paid, '$path', '$hash', $filesize, $entity, $data[order_id], $pay_block, '$pay_link', $data[InvoiceId], '$data[email_hash]', '$db_save_link')
-    ");
+    http_response_code(500);
+    echo json_encode(array(
+        'error' => 'Во время выполнения запроса произошла ошибка'
+    ));
+    return;
 }
 
-$result = $result_query->execute();
-
+http_response_code(200);
 echo json_encode(array(
     'insert-database-result' => $result,
     'link' => $link
